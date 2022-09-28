2ad94ebad516aac61a42decfcdef9c5e4a134f20
babsipearl <babsipearl@gmail.com>
2022-09-28 09:55:11 -0600

    style changes

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 'use strict';
-console.log();
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!! 🥳 ';
+console.log((document.querySelector('.number').textContent = 13));
+console.log((document.querySelector('.score').textContent = 55));
+
+document.querySelector('.guess').value = 0;
+console.log(document.querySelector('.guess').value);
